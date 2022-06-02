9d3482c4fbd74e5ef9a34ef0da98b9cc8e349b6c
danilaple <99321805+danilaple@users.noreply.github.com>
2022-06-02 12:44:08 +0300

    Create nomer4.py

---
+++ nomer4.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+def getInput():
+  return(input('запрашиваю ввод: '))
+def testInput(n):
+    try: n = int(n);
+      return(True)
+    except:
+      return(False)
+def strToInt(n):
+  return(int(n))
+def printInt(n):
+  print(n)
