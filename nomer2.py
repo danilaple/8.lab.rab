e17b88041f1b58a5212b32c0d49937f31ebfaf7a
danilaple <99321805+danilaple@users.noreply.github.com>
2022-06-02 12:42:19 +0300

    Update nomer2.py

--- nomer2.py
+++ nomer2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 def cylinder(r, h):
     from math import pi
 def circle(r): return pi*r**2
@@ -8,6 +7,5 @@ s = 2*pi*r*h
 if input('Full area? [y/n]: ') == 'y': \
 s += 2*circle(r)
 return s
-
 r, h = 1, 1
 print('s =', cylinder(r, h))
